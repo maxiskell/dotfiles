d4c2e6509a7cb2de8413a0e23a8234b86708719d
Maxi Skell <maxiskell@gmail.com>
2023-12-01 15:14:26 +0000

    nvim: nvim-tree custom mappings

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,7 +2,25 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 't', api.node.open.tab, opts 'Open: New Tab')
+  vim.keymap.set('n', 's', api.node.open.vertical, opts 'Open: Vertical Split')
+  vim.keymap.set('n', 'h', api.node.open.horizontal, opts 'Open: Horizontal Split')
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+end
+
 require('nvim-tree').setup({
+  on_attach = on_attach,
   actions = {
     open_file = {
       window_picker = {
