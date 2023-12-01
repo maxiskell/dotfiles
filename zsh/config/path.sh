dad921a4465499815503897a3a16b96eccf5eca9
Maxi Skell <maxiskell@gmail.com>
2023-12-01 15:14:28 +0000

    zsh: bun setup

--- zsh/config/path.sh
+++ zsh/config/path.sh
@@ -10,3 +10,13 @@ export PATH="$PNPM_HOME:$PATH"
 
 # Go
 export PATH="/usr/local/go/bin:$PATH"
+
+# jpeg-turbo
+export PATH="/opt/homebrew/opt/jpeg/bin:$PATH"
+
+# For compilers to find jpeg:
+export LDFLAGS="-L/opt/homebrew/opt/jpeg/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/jpeg/include"
+
+# For pkg-config to find jpeg you may need to set:
+export PKG_CONFIG_PATH="/opt/homebrew/opt/jpeg/lib/pkgconfig"
