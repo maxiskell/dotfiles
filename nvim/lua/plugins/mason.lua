e3d4c583c1ec9a96f31748c211dd6dbf77a41120
Maxi Skell <maxiskell@gmail.com>
2024-01-05 15:44:59 +0000

    nvim: add `mason-lspconfig`

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,12 @@
 return {
   "williamboman/mason.nvim",
   lazy = false,
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
+  },
   config = function()
     require("mason").setup()
+    require("mason-lspconfig").setup()
   end,
 }
