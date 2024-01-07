b0e983116752f56c79e95a9fc6c771d4ee18e7e6
Maxi Skell <maxiskell@gmail.com>
2024-01-07 17:16:43 +0000

    nvim: goodbye `lsp-zero`

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "emmet_ls",
+          "eslint",
+          "gopls",
+          "lua_ls",
+          "tsserver",
+          "tailwindcss",
+        },
+      })
+
+      require("lspconfig").eslint.setup({})
+      require("lspconfig").gopls.setup({})
+      require("lspconfig").tailwindcss.setup({})
+      require("lspconfig").tsserver.setup({})
+
+      require("lspconfig").lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      require("lspconfig").emmet_ls.setup({
+        filetypes = {
+          "css",
+          "html",
+          "javascriptreact",
+          "sass",
+          "scss",
+          "typescriptreact",
+        },
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
