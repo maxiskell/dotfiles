19de96785ae48e8079e33f6dd6517498140b8bdb
Maxi Skell <maxiskell@gmail.com>
2024-09-01 15:09:50 +0100

    zsh: linux paths

--- zsh/config/path.sh
+++ zsh/config/path.sh
@@ -1,25 +1,13 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
+# local bin
+export PATH="$PATH:$HOME/.local/bin"
+
 # neovim
 export PATH="$PATH:/opt/nvim/"
 
 # homebrew
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/opt/homebrew/bin:${PATH}"
-
-# pnpm
-export PNPM_HOME="/Users/maxi/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
 
 # Go
 export PATH="/usr/local/go/bin:$PATH"
-
-# jpeg-turbo
-export PATH="/opt/homebrew/opt/jpeg/bin:$PATH"
-
-# For compilers to find jpeg:
-export LDFLAGS="-L/opt/homebrew/opt/jpeg/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/jpeg/include"
-
-# For pkg-config to find jpeg you may need to set:
-export PKG_CONFIG_PATH="/opt/homebrew/opt/jpeg/lib/pkgconfig"
