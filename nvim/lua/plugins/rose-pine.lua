9061b5a29ff5fbbf4890090fe5dfd9a07ccba9df
Maxi Skell <maxiskell@gmail.com>
2024-09-01 15:06:16 +0100

    nvim: disable styles for `rose-pine`

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -2,6 +2,14 @@ return {
   "rose-pine/neovim",
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme rose-pine-dawn]])
+    require("rose-pine").setup({
+      variant = "dawn",
+      styles = {
+        bold = false,
+        italic = false,
+        transparency = false,
+      },
+    })
+    vim.cmd([[colorscheme rose-pine]])
   end,
 }
