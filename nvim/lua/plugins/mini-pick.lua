b73f4df0e939809222bc47716939bee95ca84881
Maxi Skell <maxiskell@gmail.com>
2025-08-11 09:34:44 +0100

    nvim: embrace the default position for mini.pick

--- nvim/lua/plugins/mini-pick.lua
+++ nvim/lua/plugins/mini-pick.lua
@@ -1,20 +1,7 @@
 return {
   "echasnovski/mini.pick",
   init = function()
-    local height = math.floor(0.618 * vim.o.lines)
-    local width = math.floor(0.618 * vim.o.columns)
-
-    require("mini.pick").setup({
-      window = {
-        config = {
-          anchor = "NW",
-          height = height,
-          width = width,
-          row = math.floor(0.5 * (vim.o.lines - height)),
-          col = math.floor(0.5 * (vim.o.columns - width)),
-        },
-      },
-    })
+    require("mini.pick").setup()
   end,
   keys = {
     {
