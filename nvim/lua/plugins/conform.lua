90d17f27f45a93e8857ab5123331ef2e88ee549e
Maxi Skell <maxiskell@gmail.com>
2025-08-24 09:45:27 +0100

    nvim: set elixir formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
+        elixir = { "mix" },
         lua = { "stylua" },
         javascript = { "prettierd" },
         javascriptreact = { "prettierd" },
