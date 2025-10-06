0fb3de7e67b58bce6907ca035fb4f82cbf22f86f
Maxi Skell <maxiskell@gmail.com>
2025-10-06 09:27:57 +0100

    nvim: load dbs for dadbod from .env

--- nvim/lua/plugins/dadbod-ui.lua
+++ nvim/lua/plugins/dadbod-ui.lua
@@ -15,10 +15,24 @@ return {
     "DBUIFindBuffer",
   },
   init = function()
-    -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+
+    local f = io.open(".env", "r")
+    if f then
+      for line in f:lines() do
+        local key, val = line:match("^([%w_]+)=(.+)$")
+        if key and val then
+          vim.env[key] = val
+        end
+      end
+      f:close()
+    end
+
+    -- Use the loaded env vars
     vim.g.dbs = {
-      -- { name = "dev", url = "" },
+      { name = "dev", url = vim.env.DB_DEV },
+      { name = "hydra_local", url = vim.env.DB_HYDRA_LOCAL },
+      { name = "prod", url = vim.env.DB_PROD },
     }
   end,
 }
