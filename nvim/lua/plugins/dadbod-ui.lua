386fee39d06d49fb0315e892a402b385aa753bfc
Maxi Skell <maxiskell@gmail.com>
2026-06-25 12:51:07 +0100

    nvim: simplify dbui url discovery

--- nvim/lua/plugins/dadbod-ui.lua
+++ nvim/lua/plugins/dadbod-ui.lua
@@ -17,22 +17,19 @@ return {
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
 
-    local f = io.open(".env", "r")
-    if f then
-      for line in f:lines() do
-        local key, val = line:match("^([%w_]+)=(.+)$")
-        if key and val then
-          vim.env[key] = val
-        end
+    local dbs = {}
+    local env = vim.fn.environ()
+
+    -- load dbs from env, based on "^DB_"-matching values
+    for key, val in pairs(env) do
+      if key:match("^DB_") and val and val ~= "" then
+        table.insert(dbs, {
+          name = key:sub(4):lower(),
+          url = val,
+        })
       end
-      f:close()
     end
 
-    -- Use the loaded env vars
-    vim.g.dbs = {
-      { name = "dev", url = vim.env.DB_DEV },
-      { name = "hydra_local", url = vim.env.DB_HYDRA_LOCAL },
-      { name = "prod", url = vim.env.DB_PROD },
-    }
+    vim.g.dbs = dbs
   end,
 }
