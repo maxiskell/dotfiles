e9a58dcf539ef3aa6223448610ecc2acd238e29b
Maxi Skell <maxiskell@gmail.com>
2026-09-14 17:15:20 +0100

    install: run link.sh under bash, not sh
    link.sh uses [[ ]] bashisms but was invoked via `sh`, which is dash on
    most Linux distros and doesn't support them.

--- install/install.sh
+++ install/install.sh
@@ -2,5 +2,5 @@
 
 BASE_DIR=$(dirname $BASH_SOURCE[0])
 
-sh $BASE_DIR/link.sh
+bash $BASE_DIR/link.sh
 sh $BASE_DIR/zsh-plugins.sh
--- install/link.sh
+++ install/link.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "\nCreating symlinks for ~./config directories"
 echo "===========================================\n"
